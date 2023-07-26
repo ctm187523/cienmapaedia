598671a3933e6477b2bf03bd3729e14396566d5d
CTM <clemente1875gmail.com>
2023-07-26 19:04:20 +0200

    Go Router Oficial Implementado, no guarda el estado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -10,24 +10,64 @@ final appRouter = GoRouter(
   initialLocation: '/',
 
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen( childView: HomeView(),),
+
+    //usamos ShellRoute en lugar de GoRoute que es mas adecuado para el uso del
+    //BottomNavigationBar ver --> https://pub.dev/documentation/go_router/latest/topics/Configuration-topic.html
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
-        //hacemos la ruta hija de la ruta principal para que siempre pueda volver a la ruta principal el usaurio desde la ruta hija
+
         GoRoute(
-          path: 'movie/:id', //tenemos un parametro en la ruta, el id de la pelicula es un String
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            //otenemos el Id de la Url usamos el state.pathParameters y de los parameters obtenemos el id
-            //en caso de que no venga mostramos el string no-id
-            final movieId = state.pathParameters['id'] ?? 'no-id';
-            return MovieScreen(movieId: movieId); 
+            return const HomeView();
           },
-       ),
+          //rutas hijas de la ruta principal al pulsar una pelicula se direcciona a los detellas dependiendo del id de la pelicula
+          routes: [
+            GoRoute(
+              path: 'movie/:id', //tenemos un parametro en la ruta, el id de la pelicula es un String
+              name: MovieScreen.name,
+              builder: (context, state) {
+                //otenemos el Id de la Url usamos el state.pathParameters y de los parameters obtenemos el id
+                //en caso de que no venga mostramos el string no-id
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId); 
+              },
+            ),
+          ]
+
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          }
+        )
       ]
-    ),
+    )
+
+    // Rutas padre/hijo
+    //LO COMENTAMOS PORQUE USAMOS EN SU LUGAR ShellRoute en el codigo de arriba
+    // GoRoute(
+    //   path: '/',
+    //   name: HomeScreen.name,
+    //   builder: (context, state) => const HomeScreen( childView: HomeView(),),
+    //   routes: [
+    //     //hacemos la ruta hija de la ruta principal para que siempre pueda volver a la ruta principal el usaurio desde la ruta hija
+    //     GoRoute(
+    //       path: 'movie/:id', //tenemos un parametro en la ruta, el id de la pelicula es un String
+    //       name: MovieScreen.name,
+    //       builder: (context, state) {
+    //         //otenemos el Id de la Url usamos el state.pathParameters y de los parameters obtenemos el id
+    //         //en caso de que no venga mostramos el string no-id
+    //         final movieId = state.pathParameters['id'] ?? 'no-id';
+    //         return MovieScreen(movieId: movieId); 
+    //       },
+    //    ),
+    //   ]
+    // ),
     
   ]
 );
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,14 +1,61 @@
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomButtomNavigation extends StatelessWidget {
+
+  //constructor
   const CustomButtomNavigation({super.key});
+
+  //metodo para saber que opcion del ButtomNavigationBar se a presionado Inicio, Categorias,Favoritos
+  //recibimos el path gracias al GoRouterState.of(context).location y con un switch devolvemos el indice
+  // del boton seleccionado 0 Inicio,1 Categorias, 2 Favoritos
+ int getCurrentIndex( BuildContext context) {
+
+    final String location = GoRouterState.of(context).location;
+
+    switch (location) {
+      case '/':
+        return 0; //no hace falta poner el break porque el return sale de la funcion
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  //metodo que da la funcionalidad cuando se presiona un bottom del ButtomNavigationBar
+  void onItemTapped ( BuildContext context, int index){
+
+    switch (index) {
+      case 0:
+        context.go('/'); //mandamos al home
+        break;
+      case 1:
+        context.go('/'); 
+        break;
+      case 2:
+        context.go('/favorites'); //mandamos a favoritos
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0, //quitamos la linea superior
       //tiene que ser minimo dos items ver video --> https://www.youtube.com/watch?v=HB5WMcxAmQQ
+     
+      //recibimos el indice del boton seleccionado /0 Inicio,1 Categorias, 2 Favoritos
+      //gracias al metodo getCurrentIndex creado arriba, y queda seleccionado el boton de la opcion elejida
+      currentIndex: getCurrentIndex(context),
+      //recibimos la opcion seleccionado del ButtomNavigationBar, en el value recibimos el numero del item seleccionado
+      //0 Inicio,1 Categorias, 2 Favoritos
+      onTap: (value){
+        onItemTapped(context, value); //llamamos a la funcion creada arriba
+      },
       items: const [
         BottomNavigationBarItem(
           icon: Icon( Icons.home_max),

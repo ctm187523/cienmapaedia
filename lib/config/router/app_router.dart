a391aa2308028c49dc0034c9ab44ba84213ed44a
CTM <clemente1875gmail.com>
2023-07-27 19:09:29 +0200

    bottom navigation con persistencia

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,13 +6,27 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
   
-  initialLocation: '/',
+  initialLocation: '/home/0',
 
+  //en esta rama fin-seccion-16-B, que es una continuacion del main, no usamos en
+  //este archivo el ShellRoute como hicimos en la rama fin-seccion-16-A que es como
+  //el equipo de Flutter recomienda si usamos un ButtonNavigationBar, pero el problema
+  //esta que al presionar un boton del menu inferior se pierde el estado si despues volvemos
+  //a la opcion que teniamos anteriormente por eso se hace de otra manera alternativa
+  //al realizado en la rama fin-seccion-16-A y asi poder mantener el estado
   routes: [
     GoRoute(
-      path: '/',
+      path: '/home/:page',//recibimos el la variable page cual es la ruta(view) que queremos recibir, home,categories,favorites
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+
+        //obtenemos el page del path (3 lineas arriba), en caso de que no venga obtenemos un String 0
+        final pageIndex = state.pathParameters['page'] ?? '0';
+
+        //devolvemos la vista que queremos visualizar(home,favorites,etc), pasandole por parametro el indice
+        //hacemos un casting de String a Int
+        return HomeScreen( pageIndex: int.parse(pageIndex));
+      },
       routes: [
         //hacemos la ruta hija de la ruta principal para que siempre pueda volver a la ruta principal el usaurio desde la ruta hija
         GoRoute(

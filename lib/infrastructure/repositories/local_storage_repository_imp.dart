e68a6f31da1b25a6be7f6012e89cc1c72869bb3e
ctm <clemente1875@gmail.com>
2023-08-20 08:07:53 +0200

    pruebas git hub desde portatil

--- lib/infrastructure/repositories/local_storage_repository_imp.dart
+++ lib/infrastructure/repositories/local_storage_repository_imp.dart
@@ -1,4 +1,6 @@
 
+//provider del repositorio local creado con Isar
+
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/local_storage_repository.dart';
